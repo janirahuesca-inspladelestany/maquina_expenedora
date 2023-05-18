b04d22f18e306fdb3cebb7f23a7b801a4bcaa0ab
4strodev <jumarurex@gmail.com>
2023-05-18 21:46:41 +0200

    feat:add metadata to application error

--- src/main/java/shared/ApplicationError.java
+++ src/main/java/shared/ApplicationError.java
@@ -3,4 +3,14 @@ package shared;
 public abstract class ApplicationError extends Exception {
     public ErrorCode errorCode;
     public String message;
+
+    private Object metadata;
+
+    public void withMetadata(Object metadata) {
+        this.metadata = metadata;
+    }
+
+    public Object getMetadata() {
+        return this.metadata;
+    }
 }
